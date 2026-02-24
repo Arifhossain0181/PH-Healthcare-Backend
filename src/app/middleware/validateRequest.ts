68fd1dc488896a0c14eb03b819789887828c5fce
Arifhossain0181 <arifhossain01816047075@gmail.com>
2026-02-24 22:51:55 +0600

    feat: add data parsing for request body in validation middleware

--- src/app/middleware/validateRequest.ts
+++ src/app/middleware/validateRequest.ts
@@ -5,6 +5,9 @@ import z from "zod";
 const validation = (zodSchema :z.ZodObject) => 
   {
      return (req:Request, res:Response, next:NextFunction) => {
+      if(req.body.data){
+        req.body = JSON.parse(req.body.data);
+      }
    const parseResult = zodSchema.safeParse(req.body);
     if(!parseResult.success){
         next(parseResult.error);
